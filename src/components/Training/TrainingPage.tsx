84036beeb0bfb99603326a621df71d25d6bb57c1
Rodnoi <coolrodnoi@mail.ru>
2024-08-18 11:52:49 +0600

    fv5

--- src/components/Training/TrainingPage.tsx
+++ src/components/Training/TrainingPage.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './TrainingPage.css';
 import Logo from '../Logo';
 
 const TrainingPage: React.FC = () => {
   const blockTitles = [
-    'Математика',
-    'Стереометрия',
-    'Логика',
-    'Мнемотехника',
-    'Игры на внимательность',
-    'Быстрые игры'
+    { title: 'Математика', path: '/Training/Math' },
+    { title: 'Стереометрия', path: '/Training/Stereometry' },
+    { title: 'Логика', path: '/Training/Logic' },
+    { title: 'Мнемотехника', path: '/Training/Mnemonics' },
+    { title: 'Игры на внимательность', path: '/Training/AttentionGames' },
+    { title: 'Быстрые игры', path: '/Training/SpeedGames' }
   ];
 
   return (
     <div className="training-page">
-        <Logo/>
+      <Logo />
       <h1 className="page-title">Тренировка</h1>
       <div className="blocks-container">
-        {blockTitles.map((title, index) => (
-          <div key={index} className="block">
-            {title}
-          </div>
+        {blockTitles.map((block, index) => (
+          <Link key={index} to={block.path} className="block">
+            {block.title}
+          </Link>
         ))}
       </div>
     </div>
